f728572974b79a3165e3f4244bd19be5c83db30a
NikBrennan <ndbrenna@lakeheadu.ca>
2023-01-10 14:21:04 -0500

    part 2 completed using p1 as starting point

---
+++ day6/day6part2.js
@@ -0,0 +1,24 @@
+/*
+Read the datasream from the input and find the first occurence of 14 unique characters back-2-back
+*/
+
+const fs = require('fs');
+const datastream = fs.readFileSync('day6/day6input.txt', 'utf8');
+// A queue is used to hold the 4 most recent values
+var queue = [];
+var markerPos = 0;
+datastream.split("").forEach((char) => {
+    queue.push(char);
+    markerPos++;
+    if (queue.length == 14) {
+        if(checkForDuplicates(queue) === false) {
+            console.log(markerPos);
+        };
+        queue.shift();
+    };
+});
+
+// Sets cannot contain duplicate values, so the sizes will be equal if there is no duplicates.
+function checkForDuplicates(queue) {
+    return new Set(queue).size !== queue.length;
+}
